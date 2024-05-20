a11f6f91c21bfba241895f31abf48f655b6c9ee1
Paul Liao <liaoyunyang0815@gmail.com>
2024-05-20 09:56:11 +0800

    dynamic green triangle

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #define GL_SILENCE_DEPRECATION
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <cmath>
 
 #include "../opengl-tools/shader.h"
 
@@ -70,6 +72,10 @@ int main(void) {
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        myShader.setFloat("greenValue", greenValue);
 
         glBindVertexArray(VAO); 
         glDrawArrays(GL_TRIANGLES, 0, 3);
--- shaders/shader.fs
+++ shaders/shader.fs
@@ -2,8 +2,8 @@
 
 out vec4 FragColor;
 
-in vec3 ourColor;
+uniform float greenValue;
 
 void main() {
-   FragColor = vec4(ourColor, 1.0f);
+   FragColor = vec4(0.0f, greenValue, 0.0f, 1.0f);
 }
